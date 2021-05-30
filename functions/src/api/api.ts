2c8ec67e75cf5e769631727d826975262701631a
Ricard Mallafre <rikitzzz@gmail.com>
2021-05-30 18:07:57 +0200

    feat: express, get/create screams, emulator import

--- functions/src/api/api.ts
+++ functions/src/api/api.ts
@@ -3,6 +3,7 @@ import * as express from 'express';
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
+const db = admin.firestore();
 const app = express();
 
 app.use(cors({ origin: true }));
@@ -15,6 +16,31 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => res.json({ message: 'api root! 🥳' }));
 
-app.get('/screams', (req, res) => res.json({ screams: [] }));
+app.get('/screams', async (req, res) => {
+  try {
+    const { docs } = await db.collection('screams').orderBy('createdAt', 'desc').get();
+    return res.json({ screams: docs.map((d) => d.data()) });
+  } catch (ex) {
+    return res.status(500).json(toJsonError(ex));
+  }
+});
 
-export { app };
+app.post('/scream', async (req, res) => {
+  try {
+    const scream = {
+      ...req.body,
+      createdAt: admin.firestore.Timestamp.now()
+    };
+
+    const result = await db.collection('screams').add(scream);
+    return res.json({ message: `scream created (${result.id})` });
+  } catch (ex) {
+    return res.status(500).json(toJsonError(ex));
+  }
+});
+
+export const api = functions.region('europe-west1').https.onRequest(app);
+
+function toJsonError(ex: Error) {
+  return { error: { name: ex.name, message: ex.message, stack: ex.stack } };
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,5 @@
 import * as admin from 'firebase-admin';
-import * as functions from 'firebase-functions';
-import { app } from './api/api';
 
 admin.initializeApp();
 
-export const api = functions.region('europe-west1').https.onRequest(app);
+export { api } from './api/api';
