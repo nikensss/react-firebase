e72b42449fa1b717381cc80a7eb466939114351a
Ricard Mallafre <rikitzzz@gmail.com>
2021-06-07 19:50:14 +0200

    feat: showing screams from db

--- socialape-client/src/App.tsx
+++ socialape-client/src/App.tsx
@@ -1,12 +1,11 @@
 import { deepPurple, purple } from '@material-ui/core/colors';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { Navbar } from './components/Navbar';
-import { home } from './pages/home';
-import { login } from './pages/login';
-import { signup } from './pages/signup';
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Signup } from './pages/Signup';
 
 const theme = createMuiTheme({
   palette: {
@@ -23,9 +22,9 @@ function App() {
           <Navbar />
           <div className='container'>
             <Switch>
-              <Route exact path='/' component={home}></Route>
-              <Route exact path='/login' component={login}></Route>
-              <Route exact path='/signup' component={signup}></Route>
+              <Route exact path='/' component={Home}></Route>
+              <Route exact path='/login' component={Login}></Route>
+              <Route exact path='/signup' component={Signup}></Route>
             </Switch>
           </div>
         </Router>
---
+++ socialape-client/src/pages/home/Home.tsx
@@ -0,0 +1,39 @@
+import { Grid } from '@material-ui/core';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './home.css';
+
+export const Home = () => {
+  const [screams, setScreams] = useState<any>([]);
+
+  useEffect(() => {
+    const getScreams = async () => {
+      const screamsRequest = await axios.get('/screams');
+      const {
+        data: { screams }
+      } = screamsRequest;
+      console.log({ screams });
+      setScreams(screams);
+    };
+
+    try {
+      getScreams();
+    } catch (ex) {
+      console.error(ex);
+    }
+  }, []);
+
+  return (
+    <Grid container spacing={6}>
+      <Grid item sm={8} xs={12}>
+        {screams.length &&
+          screams.map((s: any) => (
+            <p key={s.createdAt + s.userHandle}>{s.body}</p>
+          ))}
+      </Grid>
+      <Grid item sm={4} xs={12}>
+        <p>Profile...</p>
+      </Grid>
+    </Grid>
+  );
+};
