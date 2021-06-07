a5cfd203448175e16b580dc8cf0801c6c76b4727
Ricard Mallafre <rikitzzz@gmail.com>
2021-06-07 19:11:23 +0200

    feat: themed the app

--- socialape-client/src/App.tsx
+++ socialape-client/src/App.tsx
@@ -1,3 +1,6 @@
+import { deepPurple, purple } from '@material-ui/core/colors';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { Navbar } from './components/Navbar';
@@ -5,20 +8,29 @@ import { home } from './pages/home';
 import { login } from './pages/login';
 import { signup } from './pages/signup';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: deepPurple,
+    secondary: purple
+  }
+});
+
 function App() {
   return (
-    <div className='App'>
-      <Router>
-        <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={home}></Route>
-            <Route exact path='/login' component={login}></Route>
-            <Route exact path='/signup' component={signup}></Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className='App'>
+        <Router>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={home}></Route>
+              <Route exact path='/login' component={login}></Route>
+              <Route exact path='/signup' component={signup}></Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
